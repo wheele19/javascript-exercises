b2b25c88fd8699a92b9c52af672f79e16309c6e1
David Wheeler <wheele19@gmail.com>
2022-09-22 14:24:23 -0400

    more exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, remove) {
+    for (i = array.length - 1; i >= 0; i--) {
+        if (array[i] === remove) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let sum = 0; 
 
+  if (arg1 < 0 || arg2 < 0) return 'ERROR';
+  if (!Number.isInteger(arg2) || !Number.isInteger(arg1)) return 'ERROR';
+  
+  if (arg1 > arg2) { 
+    let length = arg1; 
+    for (i = length; i >= arg2; i--) {
+
+        sum = sum + i;
+
+    }
+    return sum;
+
+  }
+  else if (arg2 > arg1) { 
+    let length = arg2; 
+    for (i = length; i >= arg1; i--) {
+
+        sum = sum + i;
+
+    }
+    return sum;
+
+  }
+  else if (arg1 = arg2)
+    {
+      return sum = arg1;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
