5a25dce27fd899011cc32d76b024cb747256ca45
David Wheeler <wheele19@gmail.com>
2022-09-22 16:32:34 -0400

    temp conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
-
-// Do not edit below this line
-module.exports = {
-  ftoc,
-  ctof
-};
+const ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+ };
+ 
+ const ctof = function(c) {
+   return Math.round(((c * 9/5) + 32) * 10) / 10;
+ };
+ 
+ 
+ module.exports = {
+   ftoc,
+   ctof
+ };
