a9fffe9a8b5da410dec134d5c042266b36f69d28
David Wheeler <wheele19@gmail.com>
2022-09-22 16:20:22 -0400

    leap year logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,5 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+const leapYears = function(year) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+  };
+  
+  module.exports = leapYears;
