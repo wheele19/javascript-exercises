24ee7c35e06b11535a6c4d4d568564393497728a
David Wheeler <wheele19@gmail.com>
2022-09-22 08:17:39 -0400

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+
+let repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let loopstring = '';
+
+for (i=0; i < num; i++) {
+    loopstring = loopstring+string;
+}
+
+
+return loopstring;
 
 };
 
+
 // Do not edit below this line
 module.exports = repeatString;
+
